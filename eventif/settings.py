fada1cd55fa451b80227a41ce5c6e36f096c4109
juan <21133654+juanmadeira@users.noreply.github.com>
2024-10-17 00:19:57 -0300

    configuração de e-mail

--- eventif/settings.py
+++ eventif/settings.py
@@ -128,3 +128,12 @@ STATIC_ROOT = str(BASE_DIR / 'core/static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email config
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
