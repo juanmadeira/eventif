460581889656f3508a212df565a14997a4dbe445
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-11-11 14:57:42 -0300

    alterado SubscribeTest

--- subscriptions/tests.py
+++ subscriptions/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.core import mail
 from subscriptions.forms import SubscriptionForm
 
-class SubscribeTeste(TestCase):
+class SubscribeTest(TestCase):
     def setUp(self):
         self.response = self.client.get('/inscricao/')        
     
