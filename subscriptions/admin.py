ec6c9d6553f676d8c7afea2e00875b8b6d52205c
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-11-11 17:13:58 -0300

    adicionado subscription model admin

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from subscriptions.models import Subscription
 
-# Register your models here.
+admin.site.register(Subscription)
+
+class SubscriptionModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone", "cpf", "created_at")
+    date_hierarchy = "created_at"
+    search_fields = ("name", "email", )
