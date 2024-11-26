dcd85f27268035a0090b53d6b59c3845e342c6fe
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-11-26 12:19:38 -0300

    adicionado campo paid

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -4,6 +4,7 @@ from subscriptions.models import Subscription
 admin.site.register(Subscription)
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "phone", "cpf", "created_at")
+    list_display = ("name", "email", "phone", "cpf", "created_at", "paid")
     date_hierarchy = "created_at"
     search_fields = ("name", "email", )
+    list_filter = ('paid', 'created_at')
