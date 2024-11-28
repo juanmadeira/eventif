c245a23dd48f1215770bf28cd0d549366a3c818b
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-11-28 11:13:21 -0300

    atualizado modelo subscription

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Subscription(models.Model):
-    name = models.CharField('Nome', max_length=100)
+    name = models.CharField('nome', max_length=100)
     cpf = models.CharField('CPF', max_length=11)
     email = models.EmailField('e-mail')
-    phone = models.CharField('Telefone', max_length=20)
-    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    phone = models.CharField('telefone', max_length=20)
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
     paid = models.BooleanField('pago', default=False)
 
     class Meta:
