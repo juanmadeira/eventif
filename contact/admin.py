f1c9fac01e9398dd2ae7b9d3e84fe6add130ccf6
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-11-28 11:14:15 -0300

    adicionado modelo admin ao contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.utils.timezone import now
+from contact.models import Contact
 
-# Register your models here.
+class ContactModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'message', 'created_at', 'contacted_today')
+    search_fields = ('name', 'email', 'phone', 'message', 'created_at')
+    date_hierarchy = 'created_at'
+
+    def contacted_today(self, obj):
+        return obj.created_at.date() == now().date()
+
+    contacted_today.short_description = 'Enviada hoje'
+    contacted_today.boolean = True
+
+
+admin.site.register(Contact, ContactModelAdmin)
