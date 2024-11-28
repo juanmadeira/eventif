d75d43af46e4f8433154ddc1e7b73501317cd191
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-11-28 11:13:36 -0300

    atualizado modelo contact

--- contact/models.py
+++ contact/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    message = models.CharField(max_length=200)
-    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    name = models.CharField('nome', max_length=100)
+    email = models.EmailField('e-mail')
+    phone = models.CharField('telefone', max_length=20, null=True, blank=True)
+    message = models.CharField('mensagem', max_length=200)
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
 
     class Meta:
-        verbose_name = 'contato'
-        verbose_name_plural = 'contatos'
+        verbose_name = 'mensagem'
+        verbose_name_plural = 'mensagens'
         ordering = ('-created_at',)
 
     def __str__(self):
